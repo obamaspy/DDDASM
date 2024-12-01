c7f8c59868d9bcb87b407838a69bf08112ba725e
obamaspy <93859288+obamaspy@users.noreply.github.com>
2024-12-01 23:07:48 +0700

    Add files via upload

---
+++ ASM_Part2_DDD_BachPG_BH01592/ASMDDD/ASMDDD/Customers.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace ASMDDD
+{
+    partial class Customers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvcustomer = new System.Windows.Forms.DataGridView();
+            this.customerIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.aSMDataSet = new ASMDDD.ASMDataSet();
+            this.customersTableAdapter = new ASMDDD.ASMDataSetTableAdapters.CustomersTableAdapter();
+            this.btnback = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbcustomerid = new System.Windows.Forms.TextBox();
+            this.txbname = new System.Windows.Forms.TextBox();
+            this.txbphone = new System.Windows.Forms.TextBox();
+            this.txbemail = new System.Windows.Forms.TextBox();
+            this.txbaddress = new System.Windows.Forms.TextBox();
+            this.btnrefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcustomer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aSMDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvcustomer
+            // 
+            this.dgvcustomer.AutoGenerateColumns = false;
+            this.dgvcustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvcustomer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.customerIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.phoneDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn});
+            this.dgvcustomer.DataSource = this.customersBindingSource;
+            this.dgvcustomer.Location = new System.Drawing.Point(77, 303);
+            this.dgvcustomer.Name = "dgvcustomer";
+            this.dgvcustomer.RowHeadersWidth = 62;
+            this.dgvcustomer.RowTemplate.Height = 28;
+            this.dgvcustomer.Size = new System.Drawing.Size(864, 271);
+            this.dgvcustomer.TabIndex = 0;
+            this.dgvcustomer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvcustomer_CellContentClick);
+            // 
+            // customerIDDataGridViewTextBoxColumn
+            // 
+            this.customerIDDataGridViewTextBoxColumn.DataPropertyName = "CustomerID";
+            this.customerIDDataGridViewTextBoxColumn.HeaderText = "CustomerID";
+            this.customerIDDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.customerIDDataGridViewTextBoxColumn.Name = "customerIDDataGridViewTextBoxColumn";
+            this.customerIDDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // phoneDataGridViewTextBoxColumn
+            // 
+            this.phoneDataGridViewTextBoxColumn.DataPropertyName = "Phone";
+            this.phoneDataGridViewTextBoxColumn.HeaderText = "Phone";
+            this.phoneDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.phoneDataGridViewTextBoxColumn.Name = "phoneDataGridViewTextBoxColumn";
+            this.phoneDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.emailDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            this.emailDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            this.addressDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // customersBindingSource
+            // 
+            this.customersBindingSource.DataMember = "Customers";
+            this.customersBindingSource.DataSource = this.aSMDataSet;
+            // 
+            // aSMDataSet
+            // 
+            this.aSMDataSet.DataSetName = "ASMDataSet";
+            this.aSMDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customersTableAdapter
+            // 
+            this.customersTableAdapter.ClearBeforeFill = true;
+            // 
+            // btnback
+            // 
+            this.btnback.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnback.Location = new System.Drawing.Point(938, 25);
+            this.btnback.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(101, 45);
+            this.btnback.TabIndex = 1;
+            this.btnback.Text = "Back";
+            this.btnback.UseVisualStyleBackColor = true;
+            this.btnback.Click += new System.EventHandler(this.btnback_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(831, 176);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(109, 39);
+            this.btndelete.TabIndex = 8;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(650, 243);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(110, 37);
+            this.btnupdate.TabIndex = 7;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(650, 178);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(110, 37);
+            this.btnadd.TabIndex = 6;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(87, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Customer ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(87, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(87, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(87, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(87, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Address";
+            // 
+            // txbcustomerid
+            // 
+            this.txbcustomerid.Location = new System.Drawing.Point(208, 50);
+            this.txbcustomerid.Name = "txbcustomerid";
+            this.txbcustomerid.Size = new System.Drawing.Size(227, 20);
+            this.txbcustomerid.TabIndex = 15;
+            // 
+            // txbname
+            // 
+            this.txbname.Location = new System.Drawing.Point(208, 100);
+            this.txbname.Name = "txbname";
+            this.txbname.Size = new System.Drawing.Size(227, 20);
+            this.txbname.TabIndex = 16;
+            // 
+            // txbphone
+            // 
+            this.txbphone.Location = new System.Drawing.Point(208, 143);
+            this.txbphone.Name = "txbphone";
+            this.txbphone.Size = new System.Drawing.Size(227, 20);
+            this.txbphone.TabIndex = 17;
+            // 
+            // txbemail
+            // 
+            this.txbemail.Location = new System.Drawing.Point(208, 187);
+            this.txbemail.Name = "txbemail";
+            this.txbemail.Size = new System.Drawing.Size(227, 20);
+            this.txbemail.TabIndex = 18;
+            // 
+            // txbaddress
+            // 
+            this.txbaddress.Location = new System.Drawing.Point(208, 229);
+            this.txbaddress.Name = "txbaddress";
+            this.txbaddress.Size = new System.Drawing.Size(227, 20);
+            this.txbaddress.TabIndex = 19;
+            // 
+            // btnrefresh
+            // 
+            this.btnrefresh.Location = new System.Drawing.Point(831, 243);
+            this.btnrefresh.Name = "btnrefresh";
+            this.btnrefresh.Size = new System.Drawing.Size(110, 37);
+            this.btnrefresh.TabIndex = 24;
+            this.btnrefresh.Text = "Refresh";
+            this.btnrefresh.UseVisualStyleBackColor = true;
+            this.btnrefresh.Click += new System.EventHandler(this.btnrefresh_Click);
+            // 
+            // Customers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1073, 595);
+            this.Controls.Add(this.btnrefresh);
+            this.Controls.Add(this.txbaddress);
+            this.Controls.Add(this.txbemail);
+            this.Controls.Add(this.txbphone);
+            this.Controls.Add(this.txbname);
+            this.Controls.Add(this.txbcustomerid);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.dgvcustomer);
+            this.Name = "Customers";
+            this.Text = "Customers";
+            this.Load += new System.EventHandler(this.Customers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcustomer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aSMDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvcustomer;
+        private ASMDataSet aSMDataSet;
+        private System.Windows.Forms.BindingSource customersBindingSource;
+        private ASMDataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbcustomerid;
+        private System.Windows.Forms.TextBox txbname;
+        private System.Windows.Forms.TextBox txbphone;
+        private System.Windows.Forms.TextBox txbemail;
+        private System.Windows.Forms.TextBox txbaddress;
+        private System.Windows.Forms.Button btnrefresh;
+    }
+}
---
+++ ASM_Part2_DDD_BachPG_BH01592/ASMDDD/ASMDDD/Login.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ASMDDD
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.txbusername = new System.Windows.Forms.TextBox();
+            this.txbpassword = new System.Windows.Forms.TextBox();
+            this.btncreateacc = new System.Windows.Forms.Button();
+            this.btnreset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.Location = new System.Drawing.Point(403, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(188, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.Location = new System.Drawing.Point(188, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnlogin.Location = new System.Drawing.Point(192, 348);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(124, 59);
+            this.btnlogin.TabIndex = 3;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnexit.Location = new System.Drawing.Point(810, 32);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(97, 46);
+            this.btnexit.TabIndex = 4;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // txbusername
+            // 
+            this.txbusername.Location = new System.Drawing.Point(342, 224);
+            this.txbusername.Name = "txbusername";
+            this.txbusername.Size = new System.Drawing.Size(377, 20);
+            this.txbusername.TabIndex = 5;
+            // 
+            // txbpassword
+            // 
+            this.txbpassword.Location = new System.Drawing.Point(342, 281);
+            this.txbpassword.Name = "txbpassword";
+            this.txbpassword.Size = new System.Drawing.Size(377, 20);
+            this.txbpassword.TabIndex = 6;
+            // 
+            // btncreateacc
+            // 
+            this.btncreateacc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btncreateacc.Location = new System.Drawing.Point(390, 348);
+            this.btncreateacc.Name = "btncreateacc";
+            this.btncreateacc.Size = new System.Drawing.Size(124, 59);
+            this.btncreateacc.TabIndex = 7;
+            this.btncreateacc.Text = "Create Account";
+            this.btncreateacc.Click += new System.EventHandler(this.btncreateacc_Click);
+            // 
+            // btnreset
+            // 
+            this.btnreset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnreset.Location = new System.Drawing.Point(593, 348);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(124, 59);
+            this.btnreset.TabIndex = 8;
+            this.btnreset.Text = "Reset Password";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(943, 634);
+            this.Controls.Add(this.btnreset);
+            this.Controls.Add(this.btncreateacc);
+            this.Controls.Add(this.txbpassword);
+            this.Controls.Add(this.txbusername);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.TextBox txbusername;
+        private System.Windows.Forms.TextBox txbpassword;
+        private System.Windows.Forms.Button btncreateacc;
+        private System.Windows.Forms.Button btnreset;
+    }
+}
+
